1a8581b024ff9c51927182c2e6cda47d647b12ba
wjp <1060638479@qq.com>
2025-05-04 16:58:05 +0800

    feat: 后端项目初始化

---
+++ src/main/java/com/wjp/waiagentbackend/WAiAgentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wjp.waiagentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WAiAgentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WAiAgentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wjp/waiagentbackend/WAiAgentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjp.waiagentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WAiAgentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
